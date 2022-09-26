3e90a58e576f8c726f21a96013f598e55243b631
fondos2022 <114082808+fondos2022@users.noreply.github.com>
2022-09-26 14:52:56 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*
+
+Aqui van los Script necesario para la web ↓↓↓
+
+Colocar esta funcion en cada links para remplazar la pagina anterior.
+Y evitar la navegacion por el navegaros web.
+Es decir ir hacia atras y adelante con las flechas.
+
+location.replace("pagina.html");
+
+*/
